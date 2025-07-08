703da2f6069cf1e7ace906640d52947b69dfc879
Яхья <mestoev.yakhya@mail.ru>
2025-07-08 16:43:31 +0300

    feat(init): upload

---
+++ src/app/vite-env.d.ts
@@ -0,0 +1,39 @@
+declare module "*.scss" {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+declare module "*.css";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg" {
+  import React from "react";
+
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>;
+  export default SVG;
+}
+
+declare const __IS_DEV__: boolean;
+declare const __API__: string;
+declare const __PROJECT__: "storybook" | "frontend" | "jest";
+
+type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
+type DeepReadonly<T> = T extends object
+  ? {
+      readonly [P in keyof T]: DeepReadonly<T[P]>;
+    }
+  : T;
+
+type DeepRequired<T> = T extends object
+  ? {
+      [P in keyof T]-?: DeepRequired<T[P]>;
+    }
+  : T;
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+const URL = "http://localhost:8800/api";
+
+type Config = {
+  mode: "development" | "production" | "test";
+  command?: "serve" | "build";
+  isSsrBuild?: boolean;
+  isPreview?: boolean;
+};
+
+export default ({ mode }: Config) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const isDev = mode === "development";
+
+  const API_URL = env.VITE_API_URL || URL;
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: 3000,
+      open: true,
+      host: isDev ? "0.0.0.0" : "",
+    },
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+    define: {
+      __IS_DEV__: JSON.stringify(isDev),
+      __API__: JSON.stringify(API_URL),
+    },
+  });
+};
